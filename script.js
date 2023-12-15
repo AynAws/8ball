dbe7ba9ab1998d4a8adc5f51b064b81e1628f8ac
GrimCreeperyt <grimcreeperyt@gmail.com>
2023-12-15 11:46:05 -0500

    hereyougoaaron

--- script.js
+++ script.js
@@ -10,47 +10,62 @@ function shake8Ball(){
         alert('Please enter a valid question!');
         return
     }
-    const randNum = Math.floor(Math.random()*8);
+    const randNum = Math.floor(Math.random()*11);
     switch(randNum) {
         case 0:
             answer = "Yes";
-            image = "#YESIMAGE";
+            image = "imgs/green-check.png";
             color = "text-success";
             break;
         case 1:
             answer = "No";
-            image = "#NOIMAGE";
-            color = "text-success";
+            image = "imgs/Red_X.svg.png";
+            color = "text-danger";
             break;
         case 2:
             answer = "Ask again later";
-            image = "#LATERIMAGE";
-            color = "text-success";
+            image = "greygray-dot.png";
+            color = "text-muted";
             break;
         case 3:
             answer = "Maybe";
             image = "#MAYBEIMAGE";
-            color = "text-success";
+            color = "text-secondary";
             break;
         case 4:
             answer = "Cannot predict now";
             image = "#CANTIMAGE";
-            color = "text-success";
+            color = "text-muted";
             break;
         case 5:
             answer = "Don't count on it";
             image = "#UNSUREIMAGE";
-            color = "text-success";
+            color = "text-secondary";
             break;
         case 6:
             answer = "Most likely";
             image = "#LIKELYIMAGE";
-            color = "text-success";
+            color = "text-primary";
             break;
         case 7:
             answer = "Outlook not so good";
             image = "#NOTLIKELYIMAGE";
+            color = "text-warning";
+            break;
+        case 8:
+            answer = "Most definetly";
+            image = "#NOTLIKELYIMAGE";
+            color = "text-success";
+            break;
+        case 9:
+            answer = "NEVER";
+            image = "#NOTLIKELYIMAGE";
             color = "text-danger";
+            break;
+        case 10:
+            answer = "Always";
+            image = "#NOTLIKELYIMAGE";
+            color = "text-success";
             break;
         default:
             break;
