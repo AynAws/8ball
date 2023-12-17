277e485d57de871c2a91eb70e692f8d3d6bcbfb9
GrimCreeperyt <grimcreeperyt@gmail.com>
2023-12-17 12:15:38 -0500

    fixed image size by assigning classes in js

--- script.js
+++ script.js
@@ -1,77 +1,114 @@
-function shake8Ball(){
+// Triggers after clicking "shake the magic 8ball".
+function shake8Ball() {
+    // Captures the users question
     const question = prompt('Ask the Magic 8-Ball a question:');
     if (question === null) {
         document.getElementById('response-text').innerText = 'Please ask a question!';
         document.getElementById('response-text').classList = 'text-danger';
-        document.getElementById('response-image').src = '#BADIMAGE';
-        return
+        document.getElementById('response-image').src = 'imgs/Red_X.svg.png';
+        return;
     }
+    // Response if no question answered
     if (!question.trim()) {
         alert('Please enter a valid question!');
-        return
+        return;
     }
-    const randNum = Math.floor(Math.random()*11);
-    switch(randNum) {
+    let answer, image, color, alt;
+    // Generates a whole number 0-10
+    const randNum = Math.floor(Math.random() * 11);
+    // Generates one of 11 responses based off of randNum
+    switch (randNum) {
         case 0:
             answer = "Yes";
             image = "imgs/green-check.png";
             color = "text-success";
+            alt = "Green checkmark"
             break;
         case 1:
             answer = "No";
             image = "imgs/Red_X.svg.png";
             color = "text-danger";
+            alt = "Red x"
             break;
         case 2:
             answer = "Ask again later";
-            image = "greygray-dot.png";
+            image = "imgs/greygray-dot.png";
             color = "text-muted";
+            alt = "Gray dot";
             break;
         case 3:
             answer = "Maybe";
-            image = "#MAYBEIMAGE";
+            image = "imgs/da-yellow-question.png";
             color = "text-secondary";
+            alt = "Yellow questionmark"
             break;
         case 4:
             answer = "Cannot predict now";
-            image = "#CANTIMAGE";
+            image = "imgs/greygray-dot.png";
             color = "text-muted";
+            alt = "Gray dot";
             break;
         case 5:
             answer = "Don't count on it";
-            image = "#UNSUREIMAGE";
+            image = "imgs/da-yellow-question.png";
             color = "text-secondary";
+            alt = "Yellow questionmark"
             break;
         case 6:
             answer = "Most likely";
-            image = "#LIKELYIMAGE";
+            image = "imgs/da-yellow-question.png";
             color = "text-primary";
+            alt = "Yellow questionmark"
             break;
         case 7:
             answer = "Outlook not so good";
-            image = "#NOTLIKELYIMAGE";
+            image = "imgs/da-yellow-question.png";
             color = "text-warning";
+            alt = "Yellow questionmark"
             break;
         case 8:
             answer = "Most definetly";
-            image = "#NOTLIKELYIMAGE";
+            image = "imgs/green-check.png";
             color = "text-success";
+            alt = "Green checkmark"
             break;
         case 9:
             answer = "NEVER";
-            image = "#NOTLIKELYIMAGE";
+            image = "imgs/Red_X.svg.png";
             color = "text-danger";
+            alt = "Red x"
             break;
         case 10:
             answer = "Always";
-            image = "#NOTLIKELYIMAGE";
+            image = "imgs/green-check.png";
             color = "text-success";
+            alt = "Green checkmark"
             break;
         default:
+            answer = "Something went wrong, please try again.";
             break;
-    }
-    const responseText = document.getElementById('response-text');
-    responseText.innerText = answer;
-    responseText.classList = `display-4 ${color}`;
-    document.getElementById('response-text').src = image;
+}
+const responseText = document.getElementById('response-text');
+responseText.innerText = answer;
+responseText.classList = `display-4 ${color}`;
+
+// Create a new image element
+const newImage = new Image();
+newImage.onload = function() {
+    // Set the image dimensions after it has loaded
+    newImage.width = 200;
+    newImage.height = 200;
+
+    // Replace the existing image with the new one
+    const responseImage = document.getElementById('response-image');
+    responseImage.parentNode.replaceChild(newImage, responseImage);
+    newImage.className = 'mb-4 short';
+
+    const respQues = document.getElementById('resp-ques');
+    respQues.innerText = question;
+}
+
+// Set the source of the new image
+newImage.src = image;
+newImage.alt = alt;
 }
